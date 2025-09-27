3da478171286a1a893d88257468b8cf7768f1f50
Fadil <fadilvtr23@gmail.com>
2025-09-27 09:53:31 +0700

    fix config

--- Configuration.cs
+++ Configuration.cs
@@ -10,5 +10,11 @@ namespace EngineeringCoperation
     public class Configuration
     {
         public int Id { get; set; }
+        [MaxLength(4000)] public string terminologi1 { get; set; }
+        [MaxLength(4000)] public string terminologi2 { get; set; }
+        [MaxLength(4000)] public string terminologi3 { get; set; }
+        public decimal exchangeRate { get; set; }
+        public decimal transferAcrossFee { get; set; }
+        public decimal transferInhouseFee { get; set; }
     }
 }
