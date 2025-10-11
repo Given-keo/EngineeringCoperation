bb0cc4897bbcb609427e98df31110ee31cd8c397
Fadil <fadilvtr23@gmail.com>
2025-10-11 10:15:13 +0700

    create userControl

---
+++ Forms/AdminMenus/ConfigPage.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace EngineeringCoperation.Forms.AdminMenus
+{
+    partial class ConfigPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtTermin1 = new TextBox();
+            label2 = new Label();
+            txtExchange = new TextBox();
+            txtInhouse = new TextBox();
+            label3 = new Label();
+            txtTermin2 = new TextBox();
+            label4 = new Label();
+            txtAccross = new TextBox();
+            label5 = new Label();
+            txtTermin3 = new TextBox();
+            label6 = new Label();
+            btnUpdate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Terminologi 1";
+            // 
+            // txtTermin1
+            // 
+            txtTermin1.Location = new Point(29, 73);
+            txtTermin1.Multiline = true;
+            txtTermin1.Name = "txtTermin1";
+            txtTermin1.Size = new Size(215, 266);
+            txtTermin1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 363);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Exchange Rate";
+            // 
+            // txtExchange
+            // 
+            txtExchange.Location = new Point(31, 411);
+            txtExchange.Name = "txtExchange";
+            txtExchange.Size = new Size(215, 27);
+            txtExchange.TabIndex = 3;
+            txtExchange.TextChanged += textBox2_TextChanged;
+            // 
+            // txtInhouse
+            // 
+            txtInhouse.Location = new Point(328, 411);
+            txtInhouse.Name = "txtInhouse";
+            txtInhouse.Size = new Size(215, 27);
+            txtInhouse.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(328, 363);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Inhouse Fee";
+            // 
+            // txtTermin2
+            // 
+            txtTermin2.Location = new Point(326, 73);
+            txtTermin2.Multiline = true;
+            txtTermin2.Name = "txtTermin2";
+            txtTermin2.Size = new Size(215, 266);
+            txtTermin2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(328, 29);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Terminologi 2";
+            // 
+            // txtAccross
+            // 
+            txtAccross.Location = new Point(616, 411);
+            txtAccross.Name = "txtAccross";
+            txtAccross.Size = new Size(215, 27);
+            txtAccross.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(616, 363);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Accross Fee";
+            // 
+            // txtTermin3
+            // 
+            txtTermin3.Location = new Point(614, 73);
+            txtTermin3.Multiline = true;
+            txtTermin3.Name = "txtTermin3";
+            txtTermin3.Size = new Size(215, 266);
+            txtTermin3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(616, 29);
+            label6.Name = "label6";
+            label6.Size = new Size(101, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Terminologi 3";
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(383, 463);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 12;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // ConfigPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnUpdate);
+            Controls.Add(txtAccross);
+            Controls.Add(label5);
+            Controls.Add(txtTermin3);
+            Controls.Add(label6);
+            Controls.Add(txtInhouse);
+            Controls.Add(label3);
+            Controls.Add(txtTermin2);
+            Controls.Add(label4);
+            Controls.Add(txtExchange);
+            Controls.Add(label2);
+            Controls.Add(txtTermin1);
+            Controls.Add(label1);
+            Name = "ConfigPage";
+            Size = new Size(906, 516);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtTermin1;
+        private Label label2;
+        private TextBox txtExchange;
+        private TextBox txtInhouse;
+        private Label label3;
+        private TextBox txtTermin2;
+        private Label label4;
+        private TextBox txtAccross;
+        private Label label5;
+        private TextBox txtTermin3;
+        private Label label6;
+        private Button btnUpdate;
+    }
+}
