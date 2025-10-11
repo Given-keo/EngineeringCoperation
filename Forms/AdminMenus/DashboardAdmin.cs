32188169d079b837aef51f8ce44c12e4aee67749
Fadil <fadilvtr23@gmail.com>
2025-10-11 09:42:04 +0700

    push

--- Forms/AdminMenus/DashboardAdmin.cs
+++ Forms/AdminMenus/DashboardAdmin.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace EngineeringCoperation.Forms.AdminMenus
 {
-    public partial class DashboardPage : UserControl
+    public partial class DashboardAdmin : UserControl
     {
-        public DashboardPage()
+        public DashboardAdmin()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
