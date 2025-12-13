af5c6042adcc223a993ce375c0796def6a7cfaa7
Fadil <fadilvtr23@gmail.com>
2025-12-13 08:58:04 +0700

    create exchangeservice

---
+++ Services/ExchangeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineeringCoperation.Data;
+using EngineeringCoperation.Models;
+
+namespace EngineeringCoperation.Services
+{
+    public class ExchangeService
+    {
+        private readonly AppDbContext _db;
+
+        public ExchangeService(AppDbContext db) => _db = db;
+
+        public async void save(Exchange exchange) {
+            _db.Exchanges.Add(exchange);
+            await _db.SaveChangesAsync();  
+        }
+    }
+}
