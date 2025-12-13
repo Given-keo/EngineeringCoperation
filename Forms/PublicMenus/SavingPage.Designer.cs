1fa31cf428f10366032408b97518294dfdb64dd1
FeliiCode <vivianmargaret060919@gmail.com>
2025-12-13 09:41:58 +0700

    deyy

---
+++ Forms/PublicMenus/SavingPage.Designer.cs
@@ -0,0 +1,492 @@
+﻿namespace EngineeringCoperation.Forms.PublicMenus
+{
+    partial class SavingPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblSavingId = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label9 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            TxtSavingID = new TextBox();
+            TxtMinAmount = new TextBox();
+            TxtAdminFee = new TextBox();
+            TxtDueDate = new TextBox();
+            TxtInterestFine = new TextBox();
+            TxtDocKK = new TextBox();
+            TxtDocSlip = new TextBox();
+            TxtDocKtp = new TextBox();
+            TxtMaxAmount = new TextBox();
+            TxtTenor = new TextBox();
+            TxtInterest = new TextBox();
+            TxtAmount = new TextBox();
+            this.BtnBrowseKK = new Button();
+            BtnShowKK = new Button();
+            BtnShowKtp = new Button();
+            BtnBrowseKtp = new Button();
+            this.BtnShowSlip = new Button();
+            BtnBrowseSlip = new Button();
+            BtnApplySaving = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            BtnNewSaving = new Button();
+            BtnReload = new Button();
+            ComboSavingType = new ComboBox();
+            LblSign = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // LblSavingId
+            // 
+            LblSavingId.AutoSize = true;
+            LblSavingId.Location = new Point(150, 19);
+            LblSavingId.Name = "LblSavingId";
+            LblSavingId.Size = new Size(88, 25);
+            LblSavingId.TabIndex = 0;
+            LblSavingId.Text = "Saving ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(150, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Admin Fee";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(150, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Saving Type";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(150, 280);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Interest Fine";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(150, 353);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Due Date";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(150, 209);
+            label6.Name = "label6";
+            label6.Size = new Size(112, 25);
+            label6.TabIndex = 3;
+            label6.Text = "Min Amount";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(150, 414);
+            label9.Name = "label9";
+            label9.Size = new Size(120, 25);
+            label9.TabIndex = 6;
+            label9.Text = "Document KK";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(387, 414);
+            label7.Name = "label7";
+            label7.Size = new Size(163, 25);
+            label7.TabIndex = 20;
+            label7.Text = "Document Slip Gaji";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(387, 280);
+            label8.Name = "label8";
+            label8.Size = new Size(129, 25);
+            label8.TabIndex = 19;
+            label8.Text = "Document KTP";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(387, 209);
+            label11.Name = "label11";
+            label11.Size = new Size(115, 25);
+            label11.TabIndex = 17;
+            label11.Text = "Max Amount";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(387, 80);
+            label12.Name = "label12";
+            label12.Size = new Size(71, 25);
+            label12.TabIndex = 16;
+            label12.Text = "Interest";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(387, 148);
+            label13.Name = "label13";
+            label13.Size = new Size(55, 25);
+            label13.TabIndex = 15;
+            label13.Text = "Tenor";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(387, 19);
+            label14.Name = "label14";
+            label14.Size = new Size(77, 25);
+            label14.TabIndex = 14;
+            label14.Text = "Amount";
+            // 
+            // TxtSavingID
+            // 
+            TxtSavingID.Location = new Point(150, 46);
+            TxtSavingID.Name = "TxtSavingID";
+            TxtSavingID.ReadOnly = true;
+            TxtSavingID.Size = new Size(214, 31);
+            TxtSavingID.TabIndex = 21;
+            // 
+            // TxtMinAmount
+            // 
+            TxtMinAmount.Location = new Point(150, 244);
+            TxtMinAmount.Name = "TxtMinAmount";
+            TxtMinAmount.ReadOnly = true;
+            TxtMinAmount.Size = new Size(214, 31);
+            TxtMinAmount.TabIndex = 24;
+            // 
+            // TxtAdminFee
+            // 
+            TxtAdminFee.Location = new Point(150, 176);
+            TxtAdminFee.Name = "TxtAdminFee";
+            TxtAdminFee.ReadOnly = true;
+            TxtAdminFee.Size = new Size(214, 31);
+            TxtAdminFee.TabIndex = 23;
+            // 
+            // TxtDueDate
+            // 
+            TxtDueDate.Location = new Point(150, 376);
+            TxtDueDate.Name = "TxtDueDate";
+            TxtDueDate.Size = new Size(214, 31);
+            TxtDueDate.TabIndex = 26;
+            // 
+            // TxtInterestFine
+            // 
+            TxtInterestFine.Location = new Point(150, 308);
+            TxtInterestFine.Name = "TxtInterestFine";
+            TxtInterestFine.ReadOnly = true;
+            TxtInterestFine.Size = new Size(214, 31);
+            TxtInterestFine.TabIndex = 25;
+            // 
+            // TxtDocKK
+            // 
+            TxtDocKK.Location = new Point(150, 442);
+            TxtDocKK.Name = "TxtDocKK";
+            TxtDocKK.ReadOnly = true;
+            TxtDocKK.Size = new Size(214, 31);
+            TxtDocKK.TabIndex = 27;
+            // 
+            // TxtDocSlip
+            // 
+            TxtDocSlip.Location = new Point(387, 442);
+            TxtDocSlip.Name = "TxtDocSlip";
+            TxtDocSlip.ReadOnly = true;
+            TxtDocSlip.Size = new Size(214, 31);
+            TxtDocSlip.TabIndex = 33;
+            // 
+            // TxtDocKtp
+            // 
+            TxtDocKtp.Location = new Point(387, 308);
+            TxtDocKtp.Name = "TxtDocKtp";
+            TxtDocKtp.ReadOnly = true;
+            TxtDocKtp.Size = new Size(214, 31);
+            TxtDocKtp.TabIndex = 32;
+            // 
+            // TxtMaxAmount
+            // 
+            TxtMaxAmount.Location = new Point(387, 244);
+            TxtMaxAmount.Name = "TxtMaxAmount";
+            TxtMaxAmount.ReadOnly = true;
+            TxtMaxAmount.Size = new Size(214, 31);
+            TxtMaxAmount.TabIndex = 31;
+            // 
+            // TxtTenor
+            // 
+            TxtTenor.Location = new Point(387, 176);
+            TxtTenor.Name = "TxtTenor";
+            TxtTenor.ReadOnly = true;
+            TxtTenor.Size = new Size(214, 31);
+            TxtTenor.TabIndex = 30;
+            // 
+            // TxtInterest
+            // 
+            TxtInterest.Location = new Point(387, 114);
+            TxtInterest.Name = "TxtInterest";
+            TxtInterest.ReadOnly = true;
+            TxtInterest.Size = new Size(214, 31);
+            TxtInterest.TabIndex = 29;
+            // 
+            // TxtAmount
+            // 
+            TxtAmount.Location = new Point(387, 46);
+            TxtAmount.Name = "TxtAmount";
+            TxtAmount.Size = new Size(214, 31);
+            TxtAmount.TabIndex = 28;
+            // 
+            // BtnBrowseKK
+            // 
+            this.BtnBrowseKK.Location = new Point(145, 481);
+            this.BtnBrowseKK.Name = "BtnBrowseKK";
+            this.BtnBrowseKK.Size = new Size(102, 34);
+            this.BtnBrowseKK.TabIndex = 35;
+            this.BtnBrowseKK.Text = "Browse";
+            this.BtnBrowseKK.UseVisualStyleBackColor = true;
+            // 
+            // BtnShowKK
+            // 
+            BtnShowKK.Location = new Point(263, 481);
+            BtnShowKK.Name = "BtnShowKK";
+            BtnShowKK.Size = new Size(101, 34);
+            BtnShowKK.TabIndex = 36;
+            BtnShowKK.Text = "Show";
+            BtnShowKK.UseVisualStyleBackColor = true;
+            // 
+            // BtnShowKtp
+            // 
+            BtnShowKtp.Location = new Point(510, 353);
+            BtnShowKtp.Name = "BtnShowKtp";
+            BtnShowKtp.Size = new Size(101, 34);
+            BtnShowKtp.TabIndex = 38;
+            BtnShowKtp.Text = "Show";
+            BtnShowKtp.UseVisualStyleBackColor = true;
+            // 
+            // BtnBrowseKtp
+            // 
+            BtnBrowseKtp.Location = new Point(387, 348);
+            BtnBrowseKtp.Name = "BtnBrowseKtp";
+            BtnBrowseKtp.Size = new Size(102, 34);
+            BtnBrowseKtp.TabIndex = 37;
+            BtnBrowseKtp.Text = "Browse";
+            BtnBrowseKtp.UseVisualStyleBackColor = true;
+            // 
+            // BtnShowSlip
+            // 
+            this.BtnShowSlip.Location = new Point(510, 481);
+            this.BtnShowSlip.Name = "BtnShowSlip";
+            this.BtnShowSlip.Size = new Size(101, 34);
+            this.BtnShowSlip.TabIndex = 40;
+            this.BtnShowSlip.Text = "Show";
+            this.BtnShowSlip.UseVisualStyleBackColor = true;
+            // 
+            // BtnBrowseSlip
+            // 
+            BtnBrowseSlip.Location = new Point(387, 481);
+            BtnBrowseSlip.Name = "BtnBrowseSlip";
+            BtnBrowseSlip.Size = new Size(102, 34);
+            BtnBrowseSlip.TabIndex = 39;
+            BtnBrowseSlip.Text = "Browse";
+            BtnBrowseSlip.UseVisualStyleBackColor = true;
+            // 
+            // BtnApplySaving
+            // 
+            BtnApplySaving.Location = new Point(217, 545);
+            BtnApplySaving.Name = "BtnApplySaving";
+            BtnApplySaving.Size = new Size(148, 34);
+            BtnApplySaving.TabIndex = 41;
+            BtnApplySaving.Text = "Apply Saving";
+            BtnApplySaving.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(636, 46);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(517, 469);
+            dataGridView1.TabIndex = 43;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(636, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 25);
+            label1.TabIndex = 44;
+            label1.Text = "Saving";
+            // 
+            // BtnNewSaving
+            // 
+            BtnNewSaving.Location = new Point(387, 545);
+            BtnNewSaving.Name = "BtnNewSaving";
+            BtnNewSaving.Size = new Size(148, 34);
+            BtnNewSaving.TabIndex = 45;
+            BtnNewSaving.Text = "New Saving";
+            BtnNewSaving.UseVisualStyleBackColor = true;
+            // 
+            // BtnReload
+            // 
+            BtnReload.Location = new Point(626, 545);
+            BtnReload.Name = "BtnReload";
+            BtnReload.Size = new Size(148, 34);
+            BtnReload.TabIndex = 46;
+            BtnReload.Text = "Reload";
+            BtnReload.UseVisualStyleBackColor = true;
+            // 
+            // ComboSavingType
+            // 
+            ComboSavingType.FormattingEnabled = true;
+            ComboSavingType.Location = new Point(156, 112);
+            ComboSavingType.Name = "ComboSavingType";
+            ComboSavingType.Size = new Size(208, 33);
+            ComboSavingType.TabIndex = 47;
+            // 
+            // LblSign
+            // 
+            LblSign.AutoSize = true;
+            LblSign.Location = new Point(258, 18);
+            LblSign.Name = "LblSign";
+            LblSign.Size = new Size(24, 25);
+            LblSign.TabIndex = 48;
+            LblSign.Text = "...";
+            // 
+            // SavingPage
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(LblSign);
+            Controls.Add(ComboSavingType);
+            Controls.Add(BtnReload);
+            Controls.Add(BtnNewSaving);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(BtnApplySaving);
+            Controls.Add(this.BtnShowSlip);
+            Controls.Add(BtnBrowseSlip);
+            Controls.Add(BtnShowKtp);
+            Controls.Add(BtnBrowseKtp);
+            Controls.Add(BtnShowKK);
+            Controls.Add(this.BtnBrowseKK);
+            Controls.Add(TxtDocSlip);
+            Controls.Add(TxtDocKtp);
+            Controls.Add(TxtMaxAmount);
+            Controls.Add(TxtTenor);
+            Controls.Add(TxtInterest);
+            Controls.Add(TxtAmount);
+            Controls.Add(TxtDocKK);
+            Controls.Add(TxtDueDate);
+            Controls.Add(TxtInterestFine);
+            Controls.Add(TxtMinAmount);
+            Controls.Add(TxtAdminFee);
+            Controls.Add(TxtSavingID);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(label9);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(LblSavingId);
+            Name = "SavingPage";
+            Size = new Size(1182, 666);
+            Load += SavingPage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblSavingId;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label9;
+        private Label label7;
+        private Label label8;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private TextBox TxtSavingID;
+        private TextBox TxtMinAmount;
+        private TextBox TxtAdminFee;
+        private TextBox TxtDueDate;
+        private TextBox TxtInterestFine;
+        private TextBox TxtDocKK;
+        private TextBox textBox7;
+        private TextBox TxtDocSlip;
+        private TextBox TxtDocKtp;
+        private TextBox TxtMaxAmount;
+        private TextBox TxtTenor;
+        private TextBox TxtInterest;
+        private TextBox TxtAmount;
+        private Button BtnBrowse;
+        private Button BtnShow;
+        private Button BtnShowKtp;
+        private Button BtnBrowseKtp;
+        private Button BtnShowKK;
+        private Button BtnBrowseSlip;
+        private Button BtnApplySaving;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button BtnNewSaving;
+        private Button BtnReload;
+        private ComboBox ComboSavingType;
+        private Label LblSign;
+    }
+}
